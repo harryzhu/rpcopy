17e947d466634891da89e1744bddfdd9f5ca06d3
harryzhu <zhuweimin.mail@gmail.com>
2018-05-05 16:26:42 +0800

    -

--- jietu.py
+++ jietu.py
@@ -1,167 +1,179 @@
 #coding=utf-8
+"""
+jietu
+"""
 import os
 import datetime
 import time
-import yaml
 import copy
 import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 import multiprocessing
 from urllib.parse import urlparse
+import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 APP_DIR_ROOT = os.path.split(os.path.realpath(__file__))[0]
-DATA_DIR_ROOT = os.path.join(APP_DIR_ROOT,"data")
-LOGS_DIR_ROOT = os.path.join(APP_DIR_ROOT,"logs")
+DATA_DIR_ROOT = os.path.join(APP_DIR_ROOT, "data")
+LOGS_DIR_ROOT = os.path.join(APP_DIR_ROOT, "logs")
 
-USER_DATA_DIR= os.path.join(APP_DIR_ROOT,"chromeuserdatadir")
+USER_DATA_DIR = os.path.join(APP_DIR_ROOT, "chromeuserdatadir")
 
-CONF_URL_LIST = os.path.join(APP_DIR_ROOT,"config","urls.yml")
+CONF_URL_LIST = os.path.join(APP_DIR_ROOT, "config", "urls.yml")
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(level=logging.INFO)
+LOG_FORMATTER = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-logFile = os.path.join(LOGS_DIR_ROOT,"log.txt")
-rHandler = RotatingFileHandler(logFile, maxBytes=10*1024, backupCount = 3)
-rHandler.setLevel(logging.INFO)
-rHandler.setFormatter(formatter)
+LOG_FILE = os.path.join(LOGS_DIR_ROOT, "log.txt")
+FILE_HANDLER = RotatingFileHandler(LOG_FILE, maxBytes=10*1024, backupCount=3)
+FILE_HANDLER.setLevel(logging.INFO)
+FILE_HANDLER.setFormatter(LOG_FORMATTER)
 
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-console.setFormatter(formatter)
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.INFO)
+CONSOLE_HANDLER.setFormatter(LOG_FORMATTER)
 
-logger.addHandler(rHandler)
-logger.addHandler(console)
+LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(CONSOLE_HANDLER)
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--user-data-dir=" + USER_DATA_DIR)
-chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"')
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument('--ignore-certificate-errors')
+CHROME_OPTIONS = Options()
+CHROME_OPTIONS.add_argument("--headless")
+CHROME_OPTIONS.add_argument("--disable-gpu")
+CHROME_OPTIONS.add_argument("--user-data-dir=" + USER_DATA_DIR)
+CHROME_OPTIONS.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"')
+CHROME_OPTIONS.add_argument("--disable-gpu")
+CHROME_OPTIONS.add_argument('--ignore-certificate-errors')
 
-chrome_options.binary_location="/usr/bin/google-chrome-stable"
-#chrome_options.binary_location="google-chrome-stable"
+CHROME_OPTIONS.binary_location = "/usr/bin/google-chrome-stable"
+#CHROME_OPTIONS.binary_location = "google-chrome-stable"
 
-def getSnapShotByUrl(url="",fileName="",width=1920,height=5400):
-    if len(url) == 0 or len(fileName) == 0 :
-        logger.error('URL or fileName could not be empty.')
+def get_snapshot_by_url(url=None, file_name=None, width=1920, height=5400):
+    """
+    get_snapshot_by_url
+    """
+    if url is None or file_name is None:
+        LOGGER.error('URL or file_name could not be empty.')
         return None
 
-    logger.info("=====START(%s)=====" % url)
-    logger.info("URL: %s" % url)
-    NOW=datetime.datetime.now()
-    ts_suffix = "_".join([str(NOW.weekday()),str(NOW.hour)])
+    LOGGER.info("=====START(%s)=====" % url)
+    LOGGER.info("URL: %s" % url)
+    ts_now = datetime.datetime.now()
+    ts_suffix = "_".join([str(ts_now.weekday()), str(ts_now.hour)])
 
-    ws = "".join(["--window-size=",str(width),",",str(height)])
-    co = copy.deepcopy(chrome_options)
-    co.add_argument(ws)
+    window_size = "".join(["--window-size=", str(width), ",", str(height)])
+    ch_options = copy.deepcopy(CHROME_OPTIONS)
+    ch_options.add_argument(window_size)
 
-    domainName = None
-    domainName = urlparse(url).netloc
-    if not domainName.index('.'):
-        logger.error("cannot parse the URL's hostname.")
+    domain_name = None
+    domain_name = urlparse(url).netloc
+    if not domain_name.index('.'):
+        LOGGER.error("cannot parse the URL's hostname.")
         return None
         
-    domainDir = None
-    domainDir = os.path.join(DATA_DIR_ROOT,domainName)
-    if (not domainDir is None) and (not os.path.exists(domainDir)):
-        os.mkdir(domainDir)
-        logger.info('mkdir: %s' % domainDir)
+    domain_dir = None
+    domain_dir = os.path.join(DATA_DIR_ROOT, domain_name)
+    if (not domain_dir is None) and (not os.path.exists(domain_dir)):
+        os.mkdir(domain_dir)
+        LOGGER.info('mkdir: %s' % domain_dir)
 
-    if not os.path.exists(domainDir):
-        logger.error('%s does not exist.' % domainDir)
+    if not os.path.exists(domain_dir):
+        LOGGER.error('%s does not exist.' % domain_dir)
         return None
 
     if (not USER_DATA_DIR is None) and (not os.path.exists(USER_DATA_DIR)):
         os.mkdir(USER_DATA_DIR)
-        logger.info('mkdir: %s' % USER_DATA_DIR)
+        LOGGER.info('mkdir: %s' % USER_DATA_DIR)
 
     if not os.path.exists(USER_DATA_DIR):
-        logger.error('%s does not exist.' % USER_DATA_DIR)
+        LOGGER.error('%s does not exist.' % USER_DATA_DIR)
         return None
 
-    fn = "".join([domainDir,os.sep,fileName,"_",ts_suffix,".png"])
+    full_name = "".join([domain_dir, os.sep, file_name, "_", ts_suffix, ".png"])
 
     res_return = False
-    driver = webdriver.Chrome(options=co)
+    driver = webdriver.Chrome(options=ch_options)
     try:
         driver.get(url)
         driver.implicitly_wait(5)
-        driver.save_screenshot(fn)
-        if os.path.exists(fn):
+        driver.save_screenshot(full_name)
+        if os.path.exists(full_name):
             res_return = True
-    except Exception as e:
-        logger.error(e)
+    except Exception as err:
+        LOGGER.error(err)
         res_return = False
     finally:
-        logger.info("=====END(%s)=====" % url)
+        LOGGER.info("=====END(%s)=====" % url)
         driver.quit()
 
     return res_return
 
 
-def getUrlsFromYAML():
+def get_urls_from_yaml():
+    """
+    get_urls_from_yaml
+    """
     if not os.path.exists(CONF_URL_LIST):
-        logger.error('CONF_URL_LIST does not exist: %s' % CONF_URL_LIST)
+        LOGGER.error('CONF_URL_LIST does not exist: %s' % CONF_URL_LIST)
         return None
-    f = open(CONF_URL_LIST,'r',encoding='utf-8')
-    content = f.read()
-    f.close()
+    f_h = open(CONF_URL_LIST, 'r', encoding='utf-8')
+    content = f_h.read()
+    f_h.close()
     urls = yaml.safe_load(content)
     if urls is None:
-        logger.error("cannot parse the file: %s " % CONF_URL_LIST)
+        LOGGER.error("cannot parse the file: %s " % CONF_URL_LIST)
         return None
 
     urls_return = []
 
     for url in urls:
         if 'url' not in url.keys() or 'file' not in url.keys() or len(url['url']) < 1 or len(url['file']) < 1:
-            logger.error("url or file cannot be empty, will skip this line.")
+            LOGGER.error("url or file cannot be empty, will skip this line.")
             continue
 
-        winWidth = 1920
-        winHeight = 6480
+        win_width = 1920
+        win_height = 6480
         if 'width' in url.keys() and url['width'] > 0:
-            winWidth = url['width']
-        if 'height' in url.keys() and url['height'] > 0 :
-            winHeight = url['height']
+            win_width = url['width']
+        if 'height' in url.keys() and url['height'] > 0:
+            win_height = url['height']
         
-        u = {'url':url['url'], 'file':url['file'], 'width': winWidth, 'height': winHeight}
-        urls_return.append(u)
+        url_one = {'url':url['url'], 'file':url['file'], 'width': win_width, 'height': win_height}
+        urls_return.append(url_one)
 
     return urls_return
         
-def run_task(url,file,width,height):
-    logger.info('pid {0} is running, parent id is {1}, Task {2}'.format(os.getpid(), os.getppid(), url))
-    getSnapShotByUrl(url,file,width,height)
+def run_task(url, file, width, height):
+    """
+    run_task
+    """
+    LOGGER.info('pid {0} is running, parent id is {1}, Task {2}'.format(os.getpid(), os.getppid(), url))
+    get_snapshot_by_url(url, file, width, height)
 
 if __name__ == '__main__':
-    t_start = time.time()
-    argp = argparse.ArgumentParser(description='support --model.')
-    argp.add_argument('--model', default='production', help='debug / production')
-    args = argp.parse_args()
+    T_START = time.time()
+    ARGP = argparse.ArgumentParser(description='support --model.')
+    ARGP.add_argument('--model', default='production', help='debug / production')
+    ARGS = ARGP.parse_args()
     
-    urls = getUrlsFromYAML()
-    if args.model.lower() == 'debug':
-        logger.info("running model: %s ." % args.model.lower())
-        urls = urls[0:2]
-    tasks_result = []
-    if urls is not None:
-        processPool = multiprocessing.Pool(processes = 4)
+    URLS = get_urls_from_yaml()
+    if ARGS.model.lower() == 'debug':
+        LOGGER.info("running model: %s ." % ARGS.model.lower())
+        URLS = URLS[0:2]
+    TASKS_RESULT = []
+    if URLS is not None:
+        PROCESS_POOL = multiprocessing.Pool(processes=4)
 
-        for url in urls:
-            tasks_result.append(processPool.apply_async(run_task, (url['url'],url['file'],url['width'],url['height'])))
+        for url in URLS:
+            TASKS_RESULT.append(PROCESS_POOL.apply_async(run_task, (url['url'], url['file'], url['width'], url['height'])))
         
-        processPool.close()
-        processPool.join()
-        logger.info("all tasks is done.")
-        for tr in tasks_result:
-            logger.info("TASK RESULT: %s" % tr.get(timeout=1))
+        PROCESS_POOL.close()
+        PROCESS_POOL.join()
+        LOGGER.info("all tasks is done.")
+        for tr in TASKS_RESULT:
+            LOGGER.info("TASK RESULT: %s" % tr.get(timeout=1))
 
-    logger.info("Elapsed Running Time: %d seconds." % (time.time() - t_start))
+    LOGGER.info("Elapsed Running Time: %d seconds." % (time.time() - T_START))
 
--- test_jietu.py
+++ test_jietu.py
@@ -1,40 +1,56 @@
 #coding=utf-8
+"""
+jietu unittest
+"""
 import unittest
-import jietu
 import re
+import jietu
 
-def isInteger(value):
+def is_integer(value):
+    """
+    check if the param value is integer and > 0
+    """
     rec = re.compile(r'[0-9]\d*$')
     if rec.match(value):
         return True
     return False
 
 
-class jietuTestCase(unittest.TestCase):
-    def test_getUrlsFromYAML(self):
-        url_keys_required = set(['url','file','width','height'])
+class JietuTestCase(unittest.TestCase):
+    """
+    test case
+    """
+    def test_get_urls_from_yaml(self):
+        """
+        parse the yaml config
+        """
+        url_keys_required = set(['url', 'file', 'width', 'height'])
         url_key_valid = 1
         width_int_valid = 0
         height_int_valid = 0
-        urls = jietu.getUrlsFromYAML()
+        urls = jietu.get_urls_from_yaml()
         for url in urls:
             url_keys = set(url.keys())
             if not url_keys_required.issubset(url_keys):
                 url_key_valid = 0
-            if 'width' in url_keys and isInteger(str(url['width'])) and url['width'] > 0:
+            if 'width' in url_keys and is_integer(str(url['width'])) and url['width'] > 0:
                 width_int_valid = 1
-            if 'height' in url_keys and isInteger(str(url['height'])) and url['height'] > 0:
+            if 'height' in url_keys and is_integer(str(url['height'])) and url['height'] > 0:
                 height_int_valid = 1
 
-        self.assertEqual(url_key_valid, 1,'YAML configuration file cannot be parsed correctly.')
-        self.assertEqual(width_int_valid, 1,'width should be integer and > 0.')
-        self.assertEqual(height_int_valid, 1,'height should be integer and > 0.')
+        self.assertEqual(url_key_valid, 1, 'YAML configuration file cannot be parsed correctly.')
+        self.assertEqual(width_int_valid, 1, 'width should be integer and > 0.')
+        self.assertEqual(height_int_valid, 1, 'height should be integer and > 0.')
 
-    def test_getSnapShotByUrl(self):
+    def test_get_snap_shot_by_url(self):
+        """
+        try to take one snapshot by url
+        """
         #pass
         #ignore because the travis cannot have chrome and chromedriver
-        isBingImageCaptured = jietu.getSnapShotByUrl(url="https://cn.bing.com/", fileName="https_cn_bing_com", width=1920, height=1080)
-        self.assertEqual(isBingImageCaptured, True,'cannot take the screenshot.')
+        is_bing_image_captured = jietu.get_snapshot_by_url(
+            url="https://cn.bing.com/", fileName="https_cn_bing_com", width=1920, height=1080)
+        self.assertEqual(is_bing_image_captured, True, 'cannot take the screenshot.')
 
 
 
